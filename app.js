af9bfc47a36a804e8bbbe148434363db68cb54c4
Pierre Ricardin Guirand <70603318+pguirand@users.noreply.github.com>
2022-12-15 02:37:08 -0500

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-// const prompt = require('prompt-sync') ();
-
-
-console.log('test');
-// let nom = prompt("What is your name ?");
-// let age = prompt("What is your age ?");
-
-console.log('second test');
